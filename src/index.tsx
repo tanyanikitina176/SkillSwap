a611e14b819b5f0848bcc84b54e574a68959fc39
Zelenov Konstantin <KonstantinZelenovViga@yandex.com>
2025-07-26 00:09:00 +0400

    feat: add notification component with hover effects

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./app/global.css";
 import App from "./app/App.tsx";
+import { AppTry } from "./app/App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
+    <AppTry/>
   </StrictMode>,
 );
