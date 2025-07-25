3e09a00f209047c9e8a195e393c8ec893c4159ce
Nikolay Maiorov <maiorovna88@yandex.ru>
2025-07-25 16:39:50 +0300

    создад компонет Checkbox

---
+++ src/shared/ui/checkbox/type.ts
@@ -0,0 +1,9 @@
+export type CheckboxType = 'done' | 'remove'
+
+export type CheckboxProps = {
+	checkboxType: CheckboxType
+	label: string
+	disabled?: boolean
+	isChecked?: boolean
+	onChange?: (checked: boolean) => void
+}
