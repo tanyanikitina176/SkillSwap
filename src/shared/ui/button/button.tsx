4a18462740114fda7c97ebf61aa4308d12d7106a
Nikita Lunev <kasstel03@yandex.ru>
2025-07-26 18:37:51 +0300

    fix: formatting and delete empty secondary.module.css

--- src/shared/ui/button/button.tsx
+++ src/shared/ui/button/button.tsx
@@ -4,10 +4,9 @@ import type { SyntheticEvent } from "react";
 import clsx from "clsx";
 import styles from "./button.module.css";
 
-
 interface Props
   extends React.PropsWithChildren<
-    Omit<React.HTMLProps<HTMLButtonElement>, "type" >
+    Omit<React.HTMLProps<HTMLButtonElement>, "type">
   > {
   type?: "primary" | "secondary" | "tertiary";
   onClick?: (() => void) | ((e: SyntheticEvent) => void);
@@ -16,28 +15,21 @@ interface Props
 }
 
 export const Button: React.FC<Props> = ({
-
   children,
-  type = 'primary',
+  type = "primary",
   onClick,
   htmlType,
   extraClass = "",
   ...rest
 }) => {
-
   const className = clsx(
     styles.button,
     styles[`button_type_${type}`],
-    extraClass
+    extraClass,
   );
 
   return (
-    <button
-      type={htmlType}
-      onClick={onClick}
-      className={className}
-      {...rest}
-    >
+    <button type={htmlType} onClick={onClick} className={className} {...rest}>
       {children}
     </button>
   );
