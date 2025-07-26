e430600d17fdbcaec2bf38d2d28a002b6bd6aa62
Zelenov Konstantin <KonstantinZelenovViga@yandex.com>
2025-07-26 22:08:23 +0400

    fix: delete AppTry

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -10,28 +10,5 @@ function App() {
 
 export default App;
 
-import { Notification } from '../shared/ui/notification';
-
-export function AppTry() {
-  // Пример данных 
-  const notifications = [
-    { id: 1, senderName: "Олег", isRead: false },
-   /* { id: 2, senderName: "Анна", isRead: true },
-    { id: 3, senderName: "Иван", isRead: false }, */
-  ];
-
-  return (
-    <div>
-      {notifications.map((notif) => (
-        <Notification
-          key={notif.id}
-          senderName={notif.senderName}
-          onClose={() => console.log(`Закрыто уведомление ${notif.id}`)}
-          onAction={() => console.log(`Переход к обмену с ${notif.senderName}`)}
-        />
-      ))}
-    </div>
-  );
-}
 
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import './app/fonts/fonts.module.css';
 import "./app/global.css";
 import App from "./app/App.tsx";
-import { AppTry } from "./app/App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-    <AppTry/>
   </StrictMode>,
 );
