b64a8df9c6e37407fc4b0911037539c497502e04
Julia Shkolina <jsk108@yandex.ru>
2025-07-27 14:28:50 +0400

    fix: правка верстки footer под пиксель перфект с max-width: 1440px;

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,16 @@
 import "./App.css";
 import {Footer} from "../widgets/Footer/Footer.tsx";
+import {BrowserRouter} from "react-router-dom";
+
 
 function App() {
   return (
+    <BrowserRouter>
     <>
       <p>Начинаем работу</p>
       <Footer></Footer>
     </>
+    </BrowserRouter>
   );
 }
 
--- src/widgets/Footer/Footer.tsx
+++ src/widgets/Footer/Footer.tsx
@@ -1,11 +1,23 @@
 import type { FC } from "react";
 import styles from './footer.module.css';
 import { Link } from 'react-router-dom';
+import logo from '../../assets/images/logo.svg';
 
 export const Footer: FC = () => (
   <footer className={styles.footer}>
     <Link to="/" className={styles.logo}>
-      <img src="../../assets/images/logo.svg" alt="SkillSwap Logo" />
+      <img src={logo} alt="SkillSwap Logo" />
     </Link>
+
+    <nav className={styles.links}>
+      <Link to="/about">О проекте</Link>
+      <Link to="/skills">Все навыки</Link>
+      <Link to="/contacts">Контакты</Link>
+      <Link to="/blog">Блог</Link>
+      <Link to="/policy">Политика конфиденциальности</Link>
+      <Link to="/faq">Пользовательское соглашение</Link>
+    </nav>
+
+    <div className={styles.copy}>SkillSwap — 2025</div>
   </footer>
 )
