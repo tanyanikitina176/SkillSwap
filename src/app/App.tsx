f7f65b884616df8425c1d31988b3feb2065f8cca
Julia Shkolina <jsk108@yandex.ru>
2025-07-27 17:26:15 +0400

    feat: добавила компонент modal-overlay и его стили

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,18 @@
 import "./App.css";
+import {ModalOverlayUI} from "../shared/ui/modal/modal-overlay.tsx";
+
+
 
 function App() {
   return (
     <>
       <p>Начинаем работу</p>
+      <p>Начинаем работу</p>
+      <p>Начинаем работу</p>
+      <p>Начинаем работу</p>
+      <p>Начинаем работу</p>
+      <ModalOverlayUI onClick={console.log}></ModalOverlayUI>
+
     </>
   );
 }
