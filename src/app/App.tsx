13f5b0bf5aa5bc97da92c1082b48e9ba2cf7b9b2
Zelenov Konstantin <KonstantinZelenovViga@yandex.com>
2025-07-27 17:39:37 +0400

    fix: delete CategoryDisplay.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,14 @@
 import "./App.css";
 
-
-
-import { CategoryDisplay } from '../shared/lib/constants/CategoryDisplay';
-
 function App() {
   return (
-    <div>
-      <h1 style={{ textAlign: 'center' }}>Категории навыков</h1>
-      <CategoryDisplay />
-    </div>
+    <>
+      <p>Начинаем работу</p>
+    </>
   );
 }
 
 export default App;
+
+
+
