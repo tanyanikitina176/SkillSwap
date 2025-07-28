8febc3e23fb9a645799a8358d10ab95b69991d24
SP4CEVI <SP4CEVI@yandex.ru>
2025-07-28 16:37:28 +0300

    Добавил фильтры по ролям и навыкам

---
+++ src/widgets/Filtres/RoleFilter.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { RadioGroupUI } from '../../shared/ui/radiogroup/radiogroup';
+import styles from './Filtres.module.css';
+
+interface RoleFilterProps {
+  selectedRole: string;
+  onChange: (role: string) => void;
+}
+
+export const RoleFilter: React.FC<RoleFilterProps> = ({
+  selectedRole = 'Всё',
+  onChange,
+}) => {
+  const roleOptions = ['Всё', 'Хочу научиться', 'Могу научить'];
+
+  return (
+    <div className={styles.filterSection}>
+      <RadioGroupUI
+        name="role-filter"
+        options={roleOptions}
+        selected={selectedRole}
+        onChange={onChange}
+      />
+    </div>
+  );
+};
