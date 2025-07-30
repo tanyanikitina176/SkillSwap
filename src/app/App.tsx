df14ce119dd08d9963727551d9da8f57db99e5dd
Julia Shkolina <jsk108@yandex.ru>
2025-07-30 18:26:26 +0400

    feat: добавлен компонент PopularSkillList.tsx и его стили

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,270 @@
-import { BrowserRouter } from 'react-router-dom'
-import { Filtres } from '@widgets/Filtres/Filtres'
-import { Footer } from '@widgets/Footer/Footer'
-import './App.css'
-import { CategoryDisplay } from '@widgets/SkillsPanel/SkillsPanel'
-import { UserCard } from '@widgets/UserCard/user-card'
-import { demoUser as user } from '@entities/User/userHelper'
+import { BrowserRouter } from "react-router-dom";
+import { Filtres } from "@widgets/Filtres/Filtres";
+import { Footer } from "@widgets/Footer/Footer";
+import "./App.css";
+import { CategoryDisplay } from "@widgets/SkillsPanel/SkillsPanel";
+import { UserCard } from "@widgets/UserCard/user-card";
+import { demoUser as user } from "@entities/User/userHelper";
+import { PopularSkillList } from "@widgets/SkillList/PopularSkillList.tsx";
+import type {UserGender} from "@entities/User/types.ts";
+
+const demoUsers = [
+  {
+    id: "11",
+    name: "Елена",
+    age: 40,
+    gender: "female" as UserGender,
+    city: { id: "7", name: "Челябинск" },
+    wantToLearnSkills: [
+      {
+        id: "7",
+        category: {
+          id: "2",
+          name: "Творчество и искусство",
+          color: "var(--tag-creative)",
+          icon: "palette",
+        },
+        name: "Музыка и звук",
+      },
+      {
+        id: "8",
+        category: {
+          id: "2",
+          name: "Творчество и искусство",
+          color: "var(--tag-creative)",
+          icon: "palette",
+        },
+        name: "Фотография и видео",
+      },
+    ],
+    teachingSkills: [
+      {
+        id: "15",
+        category: {
+          id: "3",
+          name: "Иностранные языки",
+          color: "var(--tag-languages)",
+          icon: "global",
+        },
+        name: "Китайский язык",
+      },
+      {
+        id: "16",
+        category: {
+          id: "4",
+          name: "Образование и развитие",
+          color: "var(--tag-education)",
+          icon: "book",
+        },
+        name: "Личностное развитие",
+      },
+      {
+        id: "17",
+        category: {
+          id: "4",
+          name: "Образование и развитие",
+          color: "var(--tag-education)",
+          icon: "book",
+        },
+        name: "Психология и коммуникации",
+      },
+      {
+        id: "18",
+        category: {
+          id: "4",
+          name: "Образование и развитие",
+          color: "var(--tag-education)",
+          icon: "book",
+        },
+        name: "Обучение и преподавание",
+      },
+      {
+        id: "27",
+        category: {
+          id: "6",
+          name: "Здоровье и лайфстайл",
+          color: "var(--tag-health)",
+          icon: "lifestyle",
+        },
+        name: "Организация пространства",
+      },
+    ],
+    photo: "../db/users-photo/elena.jpg",
+    description:
+      "Нутрициолог и инструктор по йоге. Помогаю людям находить баланс между телом и разумом через правильное питание и практики йоги. Интересуюсь вопросами сна и альтернативной медициной. Научу составлять здоровое меню и освоить базовые асаны йоги.",
+    likes: [],
+  },
+  {
+    id: "11",
+    name: "Елена",
+    age: 40,
+    gender: "female" as UserGender,
+    city: { id: "7", name: "Челябинск" },
+    wantToLearnSkills: [
+      {
+        id: "7",
+        category: {
+          id: "2",
+          name: "Творчество и искусство",
+          color: "var(--tag-creative)",
+          icon: "palette",
+        },
+        name: "Музыка и звук",
+      },
+      {
+        id: "8",
+        category: {
+          id: "2",
+          name: "Творчество и искусство",
+          color: "var(--tag-creative)",
+          icon: "palette",
+        },
+        name: "Фотография и видео",
+      },
+    ],
+    teachingSkills: [
+      {
+        id: "15",
+        category: {
+          id: "3",
+          name: "Иностранные языки",
+          color: "var(--tag-languages)",
+          icon: "global",
+        },
+        name: "Китайский язык",
+      },
+      {
+        id: "16",
+        category: {
+          id: "4",
+          name: "Образование и развитие",
+          color: "var(--tag-education)",
+          icon: "book",
+        },
+        name: "Личностное развитие",
+      },
+      {
+        id: "17",
+        category: {
+          id: "4",
+          name: "Образование и развитие",
+          color: "var(--tag-education)",
+          icon: "book",
+        },
+        name: "Психология и коммуникации",
+      },
+      {
+        id: "18",
+        category: {
+          id: "4",
+          name: "Образование и развитие",
+          color: "var(--tag-education)",
+          icon: "book",
+        },
+        name: "Обучение и преподавание",
+      },
+      {
+        id: "27",
+        category: {
+          id: "6",
+          name: "Здоровье и лайфстайл",
+          color: "var(--tag-health)",
+          icon: "lifestyle",
+        },
+        name: "Организация пространства",
+      },
+    ],
+    photo: "../db/users-photo/elena.jpg",
+    description:
+      "Нутрициолог и инструктор по йоге. Помогаю людям находить баланс между телом и разумом через правильное питание и практики йоги. Интересуюсь вопросами сна и альтернативной медициной. Научу составлять здоровое меню и освоить базовые асаны йоги.",
+    likes: [],
+  },
+  {
+    id: "11",
+    name: "Елена",
+    age: 40,
+    gender: "female" as UserGender,
+    city: { id: "7", name: "Челябинск" },
+    wantToLearnSkills: [
+      {
+        id: "7",
+        category: {
+          id: "2",
+          name: "Творчество и искусство",
+          color: "var(--tag-creative)",
+          icon: "palette",
+        },
+        name: "Музыка и звук",
+      },
+      {
+        id: "8",
+        category: {
+          id: "2",
+          name: "Творчество и искусство",
+          color: "var(--tag-creative)",
+          icon: "palette",
+        },
+        name: "Фотография и видео",
+      },
+    ],
+    teachingSkills: [
+      {
+        id: "15",
+        category: {
+          id: "3",
+          name: "Иностранные языки",
+          color: "var(--tag-languages)",
+          icon: "global",
+        },
+        name: "Китайский язык",
+      },
+      {
+        id: "16",
+        category: {
+          id: "4",
+          name: "Образование и развитие",
+          color: "var(--tag-education)",
+          icon: "book",
+        },
+        name: "Личностное развитие",
+      },
+      {
+        id: "17",
+        category: {
+          id: "4",
+          name: "Образование и развитие",
+          color: "var(--tag-education)",
+          icon: "book",
+        },
+        name: "Психология и коммуникации",
+      },
+      {
+        id: "18",
+        category: {
+          id: "4",
+          name: "Образование и развитие",
+          color: "var(--tag-education)",
+          icon: "book",
+        },
+        name: "Обучение и преподавание",
+      },
+      {
+        id: "27",
+        category: {
+          id: "6",
+          name: "Здоровье и лайфстайл",
+          color: "var(--tag-health)",
+          icon: "lifestyle",
+        },
+        name: "Организация пространства",
+      },
+    ],
+    photo: "../db/users-photo/elena.jpg",
+    description:
+      "Нутрициолог и инструктор по йоге. Помогаю людям находить баланс между телом и разумом через правильное питание и практики йоги. Интересуюсь вопросами сна и альтернативной медициной. Научу составлять здоровое меню и освоить базовые асаны йоги.",
+    likes: [],
+  },
+];
 
 function App() {
   const onLikeClick = (userId: string) => {
@@ -19,6 +279,11 @@ function App() {
         <Filtres />
         <Footer />
         <CategoryDisplay />
+        <PopularSkillList
+          users={demoUsers}
+          onLikeClick={console.log}
+          onCardClick={console.log}
+        ></PopularSkillList>
       </BrowserRouter>
       <p>Начинаем работу</p>
       <UserCard
@@ -28,7 +293,6 @@ function App() {
       />
     </>
   );
-
 }
 
 export default App;
---
+++ src/widgets/SkillList/PopularSkillList.tsx
@@ -0,0 +1,45 @@
+import {type FC, useState} from "react";
+import type {User} from "@entities/User/types.ts";
+import {Button} from "@shared/ui/button/button.tsx";
+import {UserCard} from "@widgets/UserCard/user-card.tsx";
+import styles from './PopularSkilList.module.css'
+
+interface IPropsPopularSkillList {
+  users: User[];
+  onCardClick: (id: string) => void;
+  onLikeClick: (id: string) => void;
+}
+
+export const PopularSkillList: FC<IPropsPopularSkillList> = ({users, onCardClick, onLikeClick}) => {
+  const [showAll, setShowAll] = useState(false);
+
+  //заглушка если пользователей популярных - нет
+  if (!users || users.length === 0) {
+    return <p>Скоро здесь будут появятся популярные пользователи</p>;
+  }
+
+  const visiblePopularUsers = showAll ? users : users.slice(0,3)
+  return (
+  <div>
+    <section className={styles.mainSection}>
+      <div className={styles.mainSection__header}>
+        <h1 className={styles.mainSection__title}>Популярное</h1>
+        {users.length > 3 && (
+          <Button type={'tertiary'} htmlType={'button'} onClick={() => setShowAll(prev => !prev)}>{showAll ? 'Свернуть' : 'Смотреть все'}</Button>
+
+        )}
+      </div>
+      <div className={styles.grid}>
+        {visiblePopularUsers.map(user => (
+          <UserCard
+            key={user.id}
+            user={user}
+            onLikeClick={onLikeClick}
+            onButtonClick={onCardClick}
+          />
+        ))}
+      </div>
+    </section>
+  </div>
+  )
+}
