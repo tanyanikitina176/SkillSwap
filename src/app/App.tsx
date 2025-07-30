59a66194d7f66024d0273d91cdf34137ff36278e
Zelenov Konstantin <KonstantinZelenovViga@yandex.com>
2025-07-30 22:11:14 +0400

    fix:detete import

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,12 @@
 import { BrowserRouter } from 'react-router-dom'
 import './App.css'
-import { HomePage } from  '../app/pages/HomePage/HomePage'
+
 
 function App() {
 	return (
 		<>
 			<BrowserRouter>
-			<HomePage/>
+		
 			</BrowserRouter>
 		</>
 	)
