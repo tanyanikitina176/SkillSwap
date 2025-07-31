54ab9d38d738af2aef426d161e850f0aa47449e3
Aleks Shchepinov <shepinoff.sasha@yandex.ru>
2025-07-31 20:45:26 +0400

    pages/pages-404 Сделал 2 страницы для ошибок 404 и 500

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,21 @@
-import { BrowserRouter } from 'react-router-dom'
-import './App.css'
-import HomePage from '../pages/HomePage/HomePage'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import HomePage from "../pages/HomePage/HomePage";
+import { NotFound404 } from "./../pages/page-404/page-404";
+import { ConnetcError500 } from "./../pages/page-500/page-500";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <HomePage />
+        <Routes>
+			<Route path='/' element={<HomePage />} />
+			<Route path='*' element={<NotFound404 />} />
+			<Route path='/500' element={<ConnetcError500 />} />
+        </Routes>
       </BrowserRouter>
     </>
   );
-
 }
 
 export default App;
---
+++ src/pages/page-500/page-500.tsx
@@ -0,0 +1,38 @@
+import type  { FC } from "react";
+import { useNavigate } from "react-router-dom";
+import { AppHeaderUI } from "@widgets/Header";
+import { Footer } from "@widgets/Footer/Footer";
+import { Button } from "@shared/ui/button/button";
+import error_500 from "@assets/images/error_500.svg";
+import styles from "./../page-404/page-404.module.css";
+
+export const ConnetcError500: FC = () => {
+    const navigate = useNavigate();
+
+    const handleClickButton = () => {
+        navigate("/");
+    };
+
+    return (
+    <div className={styles.notFoundPage}>
+      <AppHeaderUI />
+      <main className={styles.main}>
+        <img
+          src={error_500}
+          alt="Ошибка сервера"
+        />
+        <div className={styles.containerWrap}>
+          <div className={styles.textWrap}>
+            <h2 className={styles.errorLabel}>На сервере произошла ошибка</h2>
+            <span className={styles.errorText}>
+             Попробуйте позже или вернитесь на главную страницу
+            </span>
+          </div>
+          <Button extraClass={styles.customButton} onClick={handleClickButton}>
+            <span className={styles.button}>Домой</span>
+          </Button>
+        </div>
+      </main>
+      <Footer />
+    </div>
+)};
