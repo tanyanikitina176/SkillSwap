5a9b40a3ffd822dc0e8eaa0de9ddaf176887b003
SP4CEVI <SP4CEVI@yandex.ru>
2025-07-31 13:30:13 +0300

    Поправил работу и стиль выбора даты, т.к. работало некорректно

--- src/shared/ui/data-picker/data-picker.tsx
+++ src/shared/ui/data-picker/data-picker.tsx
@@ -4,22 +4,30 @@ import { ConfigProvider, DatePicker, Space } from "antd";
 import ruRU from "antd/lib/locale/ru_RU";
 import "./data-picker.css";
 
-const onChange: DatePickerProps["onChange"] = (date, dateString) => {
-    //Тут можно добавить любое необходимое действие с датой, или получить ее из параметров
-  console.log(date, dateString);
-};
+interface DataPickerProps {
+  onChange?: (dateString: string) => void;
+}
 
-export const DataPicker: React.FC = () => (
-  <ConfigProvider locale={ruRU}>
-    <Space direction="vertical">
-      <label className="label" id="dataPicker">
-        <DatePicker
-          onChange={onChange}
-          placeholder="дд.мм.гггг"
-          className="container"
-          id="dataPicker"
-        />
-      </label>
-    </Space>
-  </ConfigProvider>
-);
+export const DataPicker: React.FC<DataPickerProps> = ({ onChange }) => {
+  const handleChange: DatePickerProps["onChange"] = (date, dateString) => {
+    if (onChange) {
+      onChange(dateString as string);
+    }
+    console.log(date, dateString);
+  };
+
+  return (
+    <ConfigProvider locale={ruRU}>
+      <Space direction="vertical">
+        <label className="label" id="dataPicker">
+          <DatePicker
+            onChange={handleChange}
+            placeholder="дд.мм.гггг"
+            className="container"
+            id="dataPicker"
+          />
+        </label>
+      </Space>
+    </ConfigProvider>
+  );
+};
