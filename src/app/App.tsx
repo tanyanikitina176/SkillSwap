06a2c720f5b3470a6f41f0c0e55b7e84c2b6d670
Tanya Nikitina <crispero7@yandex.ru>
2025-07-31 10:29:54 +0300

    добавила файл с переменными variables в App.css

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,18 @@
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
-import { UserCard } from "@widgets/UserCard/user-card";
-import { demoUser as user } from "@entities/User/userHelper";
 import NewSkillList from "@widgets/SkillList/NewSkillList";
 
 function App() {
-  /*const onLikeClick = (userId: string) => {
-    console.log(`Like clicked for user with ID: ${userId}`);
-  };
-  const onButtonClick = (userId: string) => {
-    console.log(`Button clicked for user with ID: ${userId}`);
-  };*/
   return (
     <>
       <BrowserRouter>
         <p>Начинаем работу</p>
-        <NewSkillList />
+        <NewSkillList
+          users={[]}
+          onButtonClick={() => {}}
+          onButtonMoreClick={() => {}}
+          onLikeClick={() => {}}
+        />
       </BrowserRouter>
     </>
   );
