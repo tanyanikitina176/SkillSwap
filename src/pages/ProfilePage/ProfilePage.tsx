e0d4f5e0acabcfd932071c4536139a474b6ac518
Daniil Grib <gridanil2002@gmail.com>
2025-08-06 01:28:05 +0300

    feat: добавлены Header и Footer в ProfilePage

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,13 +1,19 @@
 import { ProfileMenu } from '../../widgets/Profile/ProfileMenu';
 import { ProfileInfo } from '../../widgets/Profile/profile-info';
+import { AppHeaderUI } from '../../widgets/Header/Header';
+import { Footer } from '../../widgets/Footer/Footer';
 import styles from './ProfilePage.module.css';
 import type { FC } from 'react';
 
 export const ProfilePage: FC = () => {
   return (
-    <div className={styles.profile_page__container}>
-      <ProfileMenu />
-      <ProfileInfo />
+    <div className={styles.profile_page__wrapper}>
+      <AppHeaderUI />
+      <main className={styles.profile_page__container}>
+        <ProfileMenu />
+        <ProfileInfo />
+      </main>
+      <Footer />
     </div>
   );
 }; 
