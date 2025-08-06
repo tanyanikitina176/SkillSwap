ea6ff6874c982ede81da7db2e2bad8faf6ae12e0
Adelina Eryslanova <ravilevna.adelina@yandex.ru>
2025-08-06 16:55:47 +0500

    fix: добавление новых маршрутов, и создание защищенных

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate} from "react-router-dom";
 import "./App.css";
 import HomePage from "../pages/HomePage/HomePage";
 import { NotFound404 } from "./../pages/page-404/page-404";
@@ -8,17 +8,49 @@ import { ProfileMenu } from "@widgets/Profile/ProfileMenu";
 import { LoginPage } from "../pages/LoginPage/LoginPage.tsx";
 import { ProfileInfo } from "@widgets/Profile/profile-info.tsx";
 import { ProtectedRoute } from "./protected-route/Protected-route.tsx";
+import {ModalUI} from "@shared/ui/modal/modalUi.tsx";
 
+// import { useEffect, useState } from 'react'
+// import { fetchUsersData } from '@api/User/User-api'
+// // import type { User } from '@entities/User/types'
 
 function App() {
+
+
+  // Раскомментировать при сборки маршрутов
+  // const location = useLocation();
+  const navigate = useNavigate();
+  // const state = location.state as { backgroundLocation?: Location };
+  // const backgroundLocation = state?.backgroundLocation;
+
+  // @ts-ignore
   return (
     <>
+      {/*добавить в Route location={backgroundLocation || location}*/}
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="*" element={<NotFound404 />} />
-        <Route path="/500" element={<ConnetcError500 />} />
-        <Route path="/reg" element={<RegistrationPage />} />
-        <Route path="/login" element={<LoginPage />} />
+
+        {/* Только для НЕавторизованных*/}
+
+        <Route
+          path="/login"
+          element={
+        <ProtectedRoute onlyUnAuth >
+            <LoginPage />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/reg"
+          element={
+            <ProtectedRoute onlyUnAuth>
+            <RegistrationPage />
+                </ProtectedRoute>
+          }
+        />
+
+         Только для Авторизованных
         <Route
           path="/profile"
           element={
@@ -27,10 +59,80 @@ function App() {
             </ProtectedRoute>
           }
         />
+
+        {/*<Route*/}
+        {/*  path="/profile/favourites"*/}
+        {/*  element={*/}
+        {/*    <ProtectRouter>*/}
+        {/*      <Favourites />*/}
+        {/*    </ProtectRouter>*/}
+        {/*  }*/}
+        {/*/>*/}
+
+        <Route path="*" element={<NotFound404 />} />
+        <Route path="/500" element={<ConnetcError500 />} />
+      </Routes>
+
+      {/* Обернуть Routes в {backgroundLocation && () }*/}
+      <Routes>
+        {/* Модальное окно: Вы предложили обмен*/}
+        <Route
+          path="/proposal/sent"
+          element={
+            <ProtectedRoute>
+                <ModalUI
+                    title="Вы предложили обмен"
+                    onClose={() => navigate(-1)}
+                    image={""}
+                    imageAlt={""}
+                    description={"Теперь вы можете предложить обмен"}
+                >
+                    {/* Добавить нужный компонент */}
+                </ModalUI>
+            </ProtectedRoute>
+
+          }
+        />
+
+        {/* Модальное окно: Ваше предложение создано */}
+        <Route
+          path="/proposal/created"
+          element={
+            <ProtectedRoute>
+                <ModalUI
+                    title="Ваше предложение создано"
+                    onClose={() => navigate(-1)}
+                    image={""}
+                    imageAlt={""}
+                    description={"Теперь вы можете предложить обмен"}
+                >
+                    {/* Добавить нужный компонент */}
+                </ModalUI>
+            </ProtectedRoute>
+
+          }
+        />
+        {/*  Модальное окно: Создание предложения */}
+        <Route
+          path="/proposal/create"
+          element={
+            <ProtectedRoute>
+                <ModalUI
+                    title="При создании предложения"
+                    onClose={() => navigate(-1)}
+                    image={""}
+                    imageAlt={""}
+                    description={"Пожалуйста, проверьте и подтвердите правильность данных"}
+                >
+                    {/* Добавить нужный компонент */}
+                </ModalUI>
+            </ProtectedRoute>
+
+          }
+        />
       </Routes>
     </>
   );
-
 }
 
-export default App;
+export default App
