61cfb6bfca6c159038669616366267408c8617c3
Yulya Kazitka <effettoinduttivo@gmail.com>
2025-08-06 18:38:14 +0300

    feat: добавлен компонент HeaderLoggedIn

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,19 +1,35 @@
-import styles from "./Header.module.css";
-import logo from "@assets/images/logo.svg";
-import topic from "@assets/icons/moon.svg";
-import chevronDown from "@assets/icons/chevron-down.svg";
-import chevronUp from "@assets/icons/chevron-up.svg";
-import { SearchInputUI } from "@shared/ui/search";
-import { Button } from "@shared/ui/button/button";
-import { CategoryDisplay } from "@widgets/SkillsPanel/SkillsPanel";
-import { useRef, useState } from "react";
-import clsx from "clsx";
+import styles from './Header.module.css';
+import logo from '@assets/images/logo.svg';
+import topic from '@assets/icons/moon.svg';
+import chevronDown from '@assets/icons/chevron-down.svg';
+import chevronUp from '@assets/icons/chevron-up.svg';
+import { SearchInputUI } from '@shared/ui/search';
+import { Button } from '@shared/ui/button/button';
+import { CategoryDisplay } from '@widgets/SkillsPanel/SkillsPanel';
+import { useRef, useState } from 'react';
+import clsx from 'clsx';
 import { NavLink } from 'react-router-dom';
+import { HeaderLoggedIn } from '@shared/ui/header-logged-in/header-logged-in';
 
 export const AppHeaderUI = () => {
   const [isDropdownOpen, setDropdownOpen] = useState(false);
   const dropdownRef = useRef<HTMLDivElement>(null);
 
+  const userData = localStorage.getItem('user');
+  let userName = '';
+  let userAvatar = '';
+
+  if (userData) {
+    try {
+      const user = JSON.parse(userData);
+      userName = user.name;
+      userAvatar = user.avatar;
+    } catch (err) {
+      console.error('Ошибка при получении user из localStorage:', err);
+    }
+  }
+
+  const isAuth = !!userName;
 
   return (
     <header className={styles.header}>
@@ -24,7 +40,6 @@ export const AppHeaderUI = () => {
 
         <div className={styles.linksWrapper}>
           <span className={styles.text}>О проекте</span>
-
 
           <div className={styles.dropdownWrapper} ref={dropdownRef}>
             <button
@@ -40,33 +55,36 @@ export const AppHeaderUI = () => {
             </button>
 
             <div
-              className={`${styles.dropdownContent} ${isDropdownOpen ? styles.active : ""}`}
+              className={`${styles.dropdownContent} ${isDropdownOpen ? styles.active : ''}`}
               ref={dropdownRef}
             >
               <CategoryDisplay />
             </div>
           </div>
-
         </div>
 
         <SearchInputUI />
 
-        <div className={styles.topic}>
-          <button title="Темная тема" className={styles.topicButton}>
-            <img src={topic} alt="Тема" className={styles.icon} />
-          </button>
-        </div>
+        {isAuth ? (
+          <HeaderLoggedIn name={userName} avatar={userAvatar} />
+        ) : (
+          <>
+            <div className={styles.topic}>
+              <button title="Темная тема" className={styles.topicButton}>
+                <img src={topic} alt="Тема" className={styles.icon} />
+              </button>
+            </div>
 
-        <div className={styles.buttonsWrapper}>
-          <NavLink to="/login">
-            <Button type="secondary">Войти</Button>
-          </NavLink>
-          <NavLink to="/reg">
-            <Button type="primary">
-              Зарегистрироваться
-            </Button>
-          </NavLink>
-        </div>
+            <div className={styles.buttonsWrapper}>
+              <NavLink to="/login">
+                <Button type="secondary">Войти</Button>
+              </NavLink>
+              <NavLink to="/reg">
+                <Button type="primary">Зарегистрироваться</Button>
+              </NavLink>
+            </div>
+          </>
+        )}
       </nav>
     </header>
   );
