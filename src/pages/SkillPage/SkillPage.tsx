5326491f98cd9a57262f432ce97b230ebdc5384a
cutevil-magal <anna.magalizkaja@gmail.com>
2025-08-06 16:08:56 +0300

    feat: добавила отображение тегов

--- src/pages/SkillPage/SkillPage.tsx
+++ src/pages/SkillPage/SkillPage.tsx
@@ -6,6 +6,7 @@ import { fetchUsersData } from "../../api/User/User-api";
 import { AppHeaderUI } from '@widgets/Header';
 import styles from "./SkillPage.module.css";
 import { Footer } from '@widgets/Footer/Footer';
+import { Tag } from '@shared/ui/tag/tag';
 
 export const SkillPage = () => {
   const location = useLocation();
@@ -34,12 +35,40 @@ export const SkillPage = () => {
 
       <main className={styles.main}>
         {/* 
-        две строчки ниже после убрать или переделать
+        все что до SameOffers нужно будет убрать или переделать
         есть какая-то проблема с currentUser.name - он меняется только после обновления страницы, 
         но описание и карточки меняются как и нужно
         */}
         <h1>Профиль пользователя: {currentUser.name}</h1>
         <p>{currentUser.description}</p>
+				<div className={styles.card__skills}>
+					<span className={styles.card__skills_title}>Может научить:</span>
+					<div className={styles.card__skills_list}>
+						{currentUser.teachingSkills.length <= 2
+							? currentUser.teachingSkills.map(skill => (
+									<Tag
+										key={skill.id}
+										label={skill.name}
+										backgroundColor={skill.category.color}
+									/>
+								))
+							: currentUser.teachingSkills
+									// .slice(0, 2)
+									.map(skill => (
+										<Tag
+											key={skill.id}
+											label={skill.name}
+											backgroundColor={'#E0F7FA'}
+										/>
+									))}
+						{/* {currentUser.teachingSkills.length > 2 && (
+							<Tag
+								label={`+${currentUser.teachingSkills.length - 2}`}
+								backgroundColor={'#E8ECF7'}
+							/>
+						)} */}
+					</div>
+				</div>
         
         <SameOffers 
           users={users}
