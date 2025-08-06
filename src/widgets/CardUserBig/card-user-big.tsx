ede89f10ff61c4d90a79bbee848b4bfc79a903f5
Julia Shkolina <jsk108@yandex.ru>
2025-08-06 10:01:47 +0400

    fix: заменила title, category, subcategory,description на пропсы

--- src/widgets/CardUserBig/card-user-big.tsx
+++ src/widgets/CardUserBig/card-user-big.tsx
@@ -1,36 +1,39 @@
 import styles from "./card-user-big.module.css";
-import { Button } from "@shared/ui/button/button.tsx";
 import React, { type FC, type ReactNode } from "react";
 
 
 
 interface CardUserBigProps {
+  title: string;
+  category: string;
+  subcategory: string;
+  description: string;
   header?: React.ReactNode;
-  buttonSlot?: React.ReactNode;
+  buttonsSlot?: React.ReactNode;
   children?: React.ReactNode;
 }
 
 export const CardUserBig: FC<CardUserBigProps> = ({
+  title,
+  category,
+  subcategory,
+  description,
   header,
-  buttonSlot
+  buttonsSlot
 }) => {
   return (
     <div className={styles.bigcontainer}>
       {header}
       <div className={styles.container}>
         <div className={styles.textContainer}>
-          <h2 className={styles.title}>Игра на барабанах</h2>
+          <h2 className={styles.title}>{title}</h2>
           <p className={styles.skills}>
-            Творчество и искусство / Музыка и звук
+            {category} / {subcategory}
           </p>
           <p className={styles.description}>
-            Привет! Я играю на барабанах уже больше 10 лет — от репетиций в
-            гараже до выступлений на сцене с живыми группами. Научу основам
-            техники (и как не отбить себе пальцы), играть любимые ритмы и
-            разбирать песни, импровизировать и звучать уверенно даже без
-            паритуры
+            {description}
           </p>
-          {buttonSlot}
+          {buttonsSlot}
         </div>
         <div className={styles.containerPhoto}>
           <div className={styles.photo}></div>
