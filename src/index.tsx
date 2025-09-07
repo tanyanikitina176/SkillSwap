8dfdc903b9f34822aa66b6526a0ffb748324b75b
Tatiana Nikitina <crispero7@yandex.ru>
2025-09-07 16:51:40 +0300

    fix page

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import App from "./app/App.tsx";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <BrowserRouter basename='/SkillSwap'>
     <StrictMode>
       <App />
     </StrictMode>
